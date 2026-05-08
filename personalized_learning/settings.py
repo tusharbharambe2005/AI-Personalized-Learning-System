72c136b764625674de1c570c06daf638ec18f097
Tushar Bharambe <tusharbharambe2005@gmail.com>
2026-05-08 18:10:25 +0530

    ADD THE ALLOW HOST *

--- personalized_learning/settings.py
+++ personalized_learning/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get(
 DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
 # ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost 127.0.0.1').split()
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','ai-personalized-learning-vgyh.onrender.com']
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
