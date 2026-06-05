97cd036984797c9155233d797deebe22f9c74583
Tushar Bharambe <tusharbharambe2005@gmail.com>
2026-06-05 16:58:24 +0530

    user profile link

--- backend/personalized_learning/urls.py
+++ backend/personalized_learning/urls.py
@@ -2,9 +2,10 @@
 URL configuration for personalized_learning project.
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from learning.views import HomeView
 
 urlpatterns = [
@@ -16,4 +17,12 @@ urlpatterns = [
     path('api/auth/', include('accounts.api_urls')),
     path('api/', include('recommendations.api_urls')),
     path('api/', include('learning.api_urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
+
